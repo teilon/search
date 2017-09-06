704e50c86b656d72c951f3935a2b9e45be7715b8
teilon <teiloncw@gmail.com>
2017-09-06 17:19:48 +0600

    add cycle

--- search.py
+++ search.py
@@ -1,9 +1,16 @@
 from beau import nice_display
+from time import sleep
 import kolesa
 
 @nice_display
 def main():
-	kolesa.parse_kolesa()
+
+	while(True):
+		kolesa.parse_kolesa()
+
+		sleep(36000)
+		# sleep(120)
+
 
 
 if __name__ == '__main__':
